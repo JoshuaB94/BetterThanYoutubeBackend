3ab8174490c7e8fc767b6fde33583c9189ee373b
cFadness <codie1989@gmail.com>
2021-11-24 12:54:39 -0800

    Add comment model and MongoDB connection string. Add validation to schemas.

---
+++ models/comment.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+const Joi = require('joi');
+
+const mongoose = require('mongoose');
+
+const replySchema = new mongoose.Schema(
+    {
+        text: { type: String, required: true},
+        likes: {type: Number, default: 0},
+        dislikes: {type: Number, default: 0},
+    }
+)
+
+const commentSchema = new mongoose.Schema(
+    {
+        videoId: {type: String, required: true},
+        text: { type: String, required: true},
+        likes: {type: Number, default: 0},
+        dislikes: {type: Number, default: 0},
+        replies: {type: [replySchema], default: []}
+    }
+)
+
+function validateComment(comment) {
+    const schema = Joi.object({
+        videoId: Joi.string().required(),
+        text: Joi.string().required()
+    });
+    return schema.validate(comment);
+}
+
+function validateReply(reply) {
+    const schema = Joi.object({
+        text: Joi.string().required()
+    });
+    return schema.validate(reply);
+}
+
+const Reply = mongoose.model('Reply', replySchema);
+const Comment = mongoose.model('Comment', commentSchema);
+
+exports.Reply = Reply;
+exports.Comment = Comment;
+exports.validate = validateComment;
+exports.validate = validateReply;
+exports.commentSchema = commentSchema;
+exports.replySchema = replySchema;
