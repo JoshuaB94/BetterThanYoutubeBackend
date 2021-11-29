f5bac1a8fa61c89b0fe23b6d2fbdf3ba4b53ef71
cFadness <codie1989@gmail.com>
2021-11-29 13:25:45 -0800

    Imported CORS into index.js

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const connectDB = require('./startup/db');
 const express = require('express');
 const app = express();
 const comments = require('./routes/comments');
+const cors = require('cors');
 
 connectDB();
 
+app.use(cors());
 app.use(express.json());
 app.use('/api/comments', comments);
 
